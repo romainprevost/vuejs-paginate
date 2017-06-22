272363a9311401a909fe16a7b702c2462969a232
Romain Prevost <romain.prevost@melusim.fr>
2017-06-22 22:27:04 +0200

    Set prevText and nextText as html to allow for image links

--- src/components/Paginate.vue
+++ src/components/Paginate.vue
@@ -1,14 +1,14 @@
 <template>
   <ul :class="containerClass">
     <li :class="[prevClass, { disabled: firstPageSelected() }]">
-      <a @click="prevPage()" @keyup.enter="prevPage()" :class="prevLinkClass" tabindex="0">{{ prevText }}</a>
+      <a @click="prevPage()" @keyup.enter="prevPage()" :class="prevLinkClass" tabindex="0" v-html="prevText"></a>
     </li>
     <li v-for="page in pages" :class="[pageClass, { active: page.selected, disabled: page.disabled }]">
       <a v-if="page.disabled" :class="pageLinkClass" tabindex="0">{{ page.content }}</a>
       <a v-else @click="handlePageSelected(page.index)" @keyup.enter="handlePageSelected(page.index)" :class="pageLinkClass" tabindex="0">{{ page.content }}</a>
     </li>
     <li :class="[nextClass, { disabled: lastPageSelected() }]">
-      <a @click="nextPage()" @keyup.enter="nextPage()" :class="nextLinkClass" tabindex="0">{{ nextText }}</a>
+      <a @click="nextPage()" @keyup.enter="nextPage()" :class="nextLinkClass" tabindex="0" v-html="nextText"></a>
     </li>
   </ul>
 </template>
